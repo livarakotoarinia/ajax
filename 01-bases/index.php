a44525f19ec0dd8ba1e15bd6e54bcf3c6bddd0a6
Liva RAKOTOARINIA <liva.rakotoarinia@outlook.fr>
2019-03-11 11:04:49 +0100

    commit initial

---
+++ 01-bases/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>AJAX - Les bases</title>
+</head>
+<body>
+    <h1>Mon site</h1>
+
+    <script>
+        // On instancie le moteur AJAX
+        var xhr = new XMLHttpRequest();
+        // On veut suivre l'évolution de la requête AJAX
+        xhr.addEventListener('readystatechange', function () { 
+            if (4 === xhr.readyState) { // La requête est terminée
+                if (200 === xhr.status) { // La reponse a un status code 200
+                    // On récupère la réponse HTTP
+                    console.log(xhr.response);
+                    document.getElementsByTagName('h1')[0].innerHTML = xhr.response;
+                }
+            }
+        });
+        // On prépare une requête HTTP
+        xhr.open('GET', './worker.php');
+        // On exécute la requête HTTP
+        xhr.send();
+
+        // On récupère la réponse HTTP
+        
+    </script>
+</body>
+</html>
